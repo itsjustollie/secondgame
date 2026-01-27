759ca2c4fe786ee16619d6a127016a0002273a4b
itsjustollie <oliver.peter.anderson@gmail.com>
2026-01-27 20:11:29 +0100

    fixing animations

--- Scripts/false_human.gd
+++ Scripts/false_human.gd
@@ -5,55 +5,99 @@ extends Node
 @onready var breathing: AudioStreamPlayer2D = $Breathing
 @onready var scream: AudioStreamPlayer2D = $Scream
 
+enum State {
+	IDLE,
+	PUT_UP,
+	WAITING,
+	JUMPSCARE
+}
+
+var state: State = State.IDLE
 
 var time := 0.0
-var move_chance := 0
-var attack := false
-var wait := false
-var kill_time := 0
 var jumpscare_time := 0.0
+var kill_time := 0.0
 var aggresion := 0
 
 
-
 func _ready():
+	false_sprite.hide()
 	aggresion = Aggresion.false_ag
+	false_sprite.animation_finished.connect(_on_animation_finished)
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time += delta
-	if time >= 5.0 and attack == false:
-		move_chance = (randi_range(1, aggresion))
-		if move_chance == 1:
-			attack = true
-			breathing.play()
-			
+
+	match state:
+		State.IDLE:
+			handle_idle()
+
+		State.WAITING:
+			handle_waiting(delta)
+
+		State.JUMPSCARE:
+			handle_jumpscare(delta)
+
+
+# ---------------- STATES ----------------
+
+func handle_idle():
+	if time >= 5.0:
 		time = 0.0
-	
-	if attack == true:
-		false_sprite.play("default")
-		false_sprite.show()
-		kill_time = (randi_range(8, 13))
-		
-		if time >= kill_time:
-			attack = false
-			jumpscare_time += delta
-			breathing.stop()
-			false_sprite.play("Jumpscare")
-			scream.play()
-			wait = true
-			
-			
-		if BlinkStatus.blink_active == true:
-			if BlinkStatus.blink_time >= 3.0:
-				attack = false
-				breathing.stop()
-				leave.play()
-				false_sprite.hide()
-	
-	if wait == true:
-		jumpscare_time += delta
-		if jumpscare_time >= 1.2:
-			get_tree().change_scene_to_file("res://Scenes/die.tscn")
+		if randi_range(1, aggresion) == 1:
+			start_attack()
+
+
+func handle_waiting(delta):
+	kill_time -= delta
+
+	# Blink cancels attack
+	if BlinkStatus.blink_active and BlinkStatus.blink_time >= 3.0:
+		cancel_attack()
+		return
+
+	# Time's up → jumpscare
+	if kill_time <= 0.0:
+		start_jumpscare()
+
+
+func handle_jumpscare(delta):
+	jumpscare_time += delta
+	if jumpscare_time >= 1.2:
+		get_tree().change_scene_to_file("res://Scenes/die.tscn")
+
+
+# ---------------- ACTIONS ----------------
+
+func start_attack():
+	state = State.PUT_UP
+	false_sprite.show()
+	false_sprite.play("default") # put-up animation
+	breathing.play()
+	kill_time = randi_range(8, 9)
+
+
+func start_jumpscare():
+	state = State.JUMPSCARE
+	false_sprite.play("Jumpscare")
+	breathing.stop()
+	scream.play()
+	jumpscare_time = 0.0
+
+
+func cancel_attack():
+	state = State.IDLE
+	false_sprite.hide()
+	breathing.stop()
+	leave.play()
+	time = 0.0
+
+
+# ---------------- ANIMATION CALLBACK ----------------
+
+func _on_animation_finished():
+	match state:
+		State.PUT_UP:
+			state = State.WAITING
+			false_sprite.play("Waiting")
